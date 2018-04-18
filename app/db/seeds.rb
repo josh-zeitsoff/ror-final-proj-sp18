52141c3667834ef973b56a0c4121303f796ec3ae
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2018-04-17 18:40:18 -0700

    add seeds

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "User1", email: "user1@me.com")
+user2 = User.create(name: "User2", email: "user2@me.com")
+movie1 = Movie.create(movie_db_id: 550)
+movie2 = Movie.create(movie_db_id: 22855)
+r1 = Rating.create(thumbs_up: true, user_id: user1.id, movie_id: movie1.id)
+r2 = Rating.create(thumbs_up: false, user_id: user2.id, movie_id: movie2.id)
+
